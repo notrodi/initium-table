3d3e9d4f70986885ea12ec7d74f0a5686df61019
notrodi <nnotrodi1@gmail.com>
2024-03-04 15:27:37 +0300

    added validators

--- src/app/modules/dialog/dialog.component.ts
+++ src/app/modules/dialog/dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Output, ViewEncapsulation } from '@angular/cor
 import { DialogService } from 'app/modules/dialog/services/dialog.service';
 import { ClientsRemovalService } from '../clients/services/clients-removal.service';
 import { dialogType } from './models/dialog-type.enum';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngxs/store';
 import { ClientsActions } from '../clients/state/clients.actions';
 import { ClientsState } from '../clients/state/clients.state';
@@ -15,6 +15,7 @@ import { ClientsState } from '../clients/state/clients.state';
   encapsulation: ViewEncapsulation.None
 })
 export class DialogComponent {
+  phoneRegExp: RegExp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
   newClientForm: FormGroup;
   dialogRemove = dialogType.REMOVE;
   dialogAdd = dialogType.ADD;
@@ -42,10 +43,19 @@ export class DialogComponent {
 
   ngOnInit(): void {
     this.newClientForm = this._fb.group({
-      name: [null],
-      surname: [null],
-      email: [null],
-      phone: [null]
+      name: [null, [
+        Validators.required,
+        Validators.minLength(2)
+      ]],
+      surname: [null, [
+        Validators.required,
+        Validators.minLength(2)
+      ]],
+      email: [null, [
+        Validators.required,
+        Validators.email
+      ]],
+      phone: [null, Validators.pattern(this.phoneRegExp)]
     })
 
     if (this.clientIndex >= 0) {
@@ -55,18 +65,34 @@ export class DialogComponent {
     }
   }
 
+  public isControlInvalid(controlName: string): boolean {
+    const control = this.newClientForm.controls[controlName];
+  
+    return control.invalid && control.touched;
+  }
+
   public onClose(): void {
     this._dialog.close();
   }
 
   public confirm(): void {
-    if (this.dialogType === dialogType.ADD) {
-      this._store.dispatch(new ClientsActions.AddNewClient(this.newClientForm.value));
-    }
-
-    if (this.dialogType === dialogType.EDIT) {
-      this._store.dispatch(new ClientsActions.EditClient(this.clientIndex, this.newClientForm.value));
-      this.newClientForm.reset();
+    if(this.dialogType === dialogType.ADD || this.dialogType === dialogType.EDIT) {
+      const controls = this.newClientForm.controls;
+  
+      if (this.newClientForm.invalid) {
+        Object.keys(controls).forEach(controlName => controls[controlName].markAsTouched());
+        
+        return;
+      }
+  
+      if (this.dialogType === dialogType.ADD) {
+        this._store.dispatch(new ClientsActions.AddNewClient(this.newClientForm.value));
+      }
+  
+      if (this.dialogType === dialogType.EDIT) {
+        this._store.dispatch(new ClientsActions.EditClient(this.clientIndex, this.newClientForm.value));
+        this.newClientForm.reset();
+      }
     }
 
     this._dialog.close();
